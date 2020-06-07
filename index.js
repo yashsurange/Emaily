5e1612137c092094093821dde3ee6b62306ecb4d
Yash Surange <yashsurangeyash@yahoo.com>
2020-06-07 22:50:43 +0530

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const app=express();// first application (all the router handelers will be associated with this app)
 
 app.get('/',(req,res)=> {     // route handler
-  res.send({hi:'there'});     // json data
+  res.send({bye:'buddy '});     // json data
 });
 
 const PORT= process.env.PORT || 5000; // dynamic binding(port on heroku) or just the default port.
